f99c19166450410865e113f3af78a33c3fd84bcf
Ogunleye Oluwatosin <ogunleyeoluwatosin2014@gmail.com>
2021-05-04 20:23:00 +0100

    My blog

---
+++ config/blog/tests.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from .models import Post
+# Create your tests here.
+
+class BlogTests(TestCase):
+    def setup(self):
+        self.user = get_user_model().objects.create_user(
+            username = 'testuser',
+            email = 'test@gmail.com',
+            password = 'secret'
+        )
+        self.post = Post.objects.create(
+            title = 'A good title',
+            body = 'Nice Body',
+            author = self.user
+        )
+    
+    def test_string_representation(self):
+        post = Post(title = 'A simple title')
+        self.assertEqual(str(post), post.title)
+    
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A good title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Nice Body')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice Body')
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_post_detail_views(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A good title')
+        self.assertTemplateUsed(response, 'post_detail.html')
